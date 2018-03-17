b5db596c3c7fe10aeb23465485603870e7cd7bd3
nhom2-ltdd2 <nhom2.ltdd2@gmail.com>
2018-03-17 20:22:50 +0700

    Cập nhật Lock Screen

--- NhatKy/app/src/main/java/vn/edu/tdc/nhom2/nhatky/Activities/LockActivity.java
+++ NhatKy/app/src/main/java/vn/edu/tdc/nhom2/nhatky/Activities/LockActivity.java
@@ -3,9 +3,12 @@ package vn.edu.tdc.nhom2.nhatky.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
 
 import java.util.List;
 
@@ -14,6 +17,10 @@ import vn.edu.tdc.nhom2.nhatky.R;
 
 public class LockActivity extends AppCompatActivity {
     PatternLockView patternLockView;
+    Button btn_next;
+    String password = "";
+    int step = 0;
+
     PatternLockViewListener patternLockViewListener = new PatternLockViewListener() {
         @Override
         public void onStarted() {
@@ -27,12 +34,23 @@ public class LockActivity extends AppCompatActivity {
 
         @Override
         public void onComplete(List<PatternLockView.Dot> pattern) {
-
+            switch (step) {
+                case 0:
+                    password = PatternLockUtils.patternToString(patternLockView, pattern);
+                    break;
+                case 1:
+                    if (password == PatternLockUtils.patternToString(patternLockView, pattern)) {
+                        btn_next.setText("HOÀN TẤT");
+                    } else {
+                        patternLockView.setWrongStateColor(patternLockView.getWrongStateColor());
+                    }
+                    break;
+            }
         }
 
         @Override
         public void onCleared() {
-
+            password = "";
         }
     };
 
@@ -42,6 +60,22 @@ public class LockActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         patternLockView = findViewById(R.id.pattern_lock_view);
         patternLockView.addPatternLockListener(patternLockViewListener);
-        Log.d("test", Preferences.getLockCode(this));
+        btn_next = findViewById(R.id.btn_next);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test", "Cli");
+                switch (step) {
+                    case 0:
+                        patternLockView.clearPattern();
+                        step++;
+                        break;
+                    case 1:
+
+                        break;
+                }
+            }
+        });
     }
 }
