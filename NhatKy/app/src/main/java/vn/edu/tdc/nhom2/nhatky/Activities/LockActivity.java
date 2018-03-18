9b4e6e018698733982dcac1d8996647a242e7a71
nhom2-ltdd2 <nhom2.ltdd2@gmail.com>
2018-03-18 11:55:46 +0700

    Cập nhật LockScreen, README

--- NhatKy/app/src/main/java/vn/edu/tdc/nhom2/nhatky/Activities/LockActivity.java
+++ NhatKy/app/src/main/java/vn/edu/tdc/nhom2/nhatky/Activities/LockActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
@@ -17,7 +18,8 @@ import vn.edu.tdc.nhom2.nhatky.R;
 
 public class LockActivity extends AppCompatActivity {
     PatternLockView patternLockView;
-    Button btn_next;
+    TextView txt_info;
+    Button btn_next, btn_prev;
     String password = "";
     int step = 0;
 
@@ -36,13 +38,29 @@ public class LockActivity extends AppCompatActivity {
         public void onComplete(List<PatternLockView.Dot> pattern) {
             switch (step) {
                 case 0:
-                    password = PatternLockUtils.patternToString(patternLockView, pattern);
+                    if (PatternLockUtils.patternToString(patternLockView, pattern).length() > 3) {
+                        password = PatternLockUtils.patternToString(patternLockView, pattern);
+                        txt_info.setText(R.string.lock_txt_no);
+                        btn_next.setEnabled(true);
+                    } else {
+                        txt_info.setText(R.string.lock_txt_length);
+                        patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+                    }
                     break;
                 case 1:
-                    if (password == PatternLockUtils.patternToString(patternLockView, pattern)) {
-                        btn_next.setText("HOÀN TẤT");
+                    if (password.equals(PatternLockUtils.patternToString(patternLockView, pattern))) {
+                        btn_next.setEnabled(true);
                     } else {
-                        patternLockView.setWrongStateColor(patternLockView.getWrongStateColor());
+                        txt_info.setText(R.string.lock_txt_err);
+                        patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+                    }
+                    break;
+                case 2:
+                    if (password.equals(PatternLockUtils.patternToString(patternLockView, pattern))) {
+                        Log.d("check", "success");
+                    } else {
+                        txt_info.setText(R.string.lock_txt_ne);
+                        patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
                     }
                     break;
             }
@@ -50,7 +68,21 @@ public class LockActivity extends AppCompatActivity {
 
         @Override
         public void onCleared() {
-            password = "";
+            switch (step) {
+                case 0:
+                    password = "";
+                    btn_next.setEnabled(false);
+                    txt_info.setText(R.string.lock_txt_no);
+                    break;
+                case 1:
+                    btn_next.setEnabled(false);
+                    txt_info.setText(R.string.lock_txt_next);
+                    break;
+                case 2:
+                    txt_info.setText(R.string.lock_txt_yes);
+                    break;
+            }
+
         }
     };
 
@@ -61,20 +93,49 @@ public class LockActivity extends AppCompatActivity {
         patternLockView = findViewById(R.id.pattern_lock_view);
         patternLockView.addPatternLockListener(patternLockViewListener);
         btn_next = findViewById(R.id.btn_next);
+        btn_prev = findViewById(R.id.btn_prev);
+        txt_info = findViewById(R.id.txt_info);
+        String pass = Preferences.getLockCode(this);
+        if (!pass.equals("Null")) {
+            password = pass;
+            txt_info.setText(R.string.lock_txt_yes);
+            btn_next.setVisibility(View.INVISIBLE);
+            step = 2;
+
+        }
 
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("test", "Cli");
                 switch (step) {
                     case 0:
-                        patternLockView.clearPattern();
-                        step++;
+                        if (!password.equals("")) {
+                            patternLockView.clearPattern();
+                            txt_info.setText(R.string.lock_txt_next);
+                            btn_prev.setVisibility(View.VISIBLE);
+                            btn_next.setText(R.string.lock_btn_fin);
+                            btn_next.setEnabled(false);
+                            step++;
+                        }
                         break;
                     case 1:
-
+                        Preferences.setLockCode(LockActivity.this, password);
+                        Log.d("check", "cahnge");
                         break;
                 }
+            }
+        });
+
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step = 0;
+                password = "";
+                txt_info.setText(R.string.lock_txt_no);
+                btn_next.setText(R.string.lock_btn_next);
+                btn_prev.setVisibility(View.INVISIBLE);
+                btn_next.setEnabled(false);
+                patternLockView.clearPattern();
             }
         });
     }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DoAn-NhatKy
+## Danh sách thành viên nhóm 2
+* Huỳnh Nhật Minh Tiến (Leader)
+* Nguyễn Cao Phước (Tester)
+* Nguyễn Đức Lai
+* Nguyễn Đăng Trường
+* Trần Cao Vinh
+* Trần Hồng Đức
