b04424f40045cd6fa822181654d2e04bad153c57
truong2112 <nguyendangtruong259@gmail.com>
2018-05-12 11:05:25 +0700

    liên kết màn hình about

--- ColorBubble/app/src/main/java/vn/edu/tdc/nhom2/colorbubble/HomeActivity.java
+++ ColorBubble/app/src/main/java/vn/edu/tdc/nhom2/colorbubble/HomeActivity.java
@@ -4,8 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button Start_btn;
+    private Button Ranking_btn;
+    private Button Setting_btn;
+    private Button About_btn;
+    private Button Exit_btn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +25,60 @@ public class HomeActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        Start_btn = (Button) findViewById(R.id.Start_btn);
+        Ranking_btn = (Button) findViewById(R.id.Ranking_btn) ;
+        Setting_btn = (Button) findViewById(R.id.Setting_btn) ;
+        About_btn = (Button) findViewById(R.id.About_btn) ;
+        Exit_btn = (Button) findViewById(R.id.Exit_btn) ;
 
+
+
+        Start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, WebViewActivity.class );
+                startActivity(intent);
+            }
+
+
+        });
+        Ranking_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Ranking.class );
+                startActivity(intent);
+            }
+
+
+        });
+        Setting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, SettingActivity.class );
+                startActivity(intent);
+            }
+
+
+        });
+        About_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, AboutActivity.class );
+                startActivity(intent);
+            }
+
+
+        });
+
+        Exit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Gameover.class );
+                startActivity(intent);
+            }
+
+
+        });
 
     }
 }
